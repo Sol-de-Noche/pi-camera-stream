8f8b7af551178f1ee86a985537d3275da72879f1
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2021-09-02 16:38:07 +0200

    Added API for control the server

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Desc: This web application serves a motion JPEG stream
 # main.py
 # import the necessary packages
+from pydantic import BaseModel
 import os
 import math
 import time
@@ -10,37 +11,69 @@ import threading
 from multiprocessing import Process, Queue
 from timeit import default_timer as timer
 from fastapi import FastAPI, Request
+from typing import List, Optional
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from starlette.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
+
 from camera import VideoCamera
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
+class StatusType(BaseModel):
+    status: bool
+
+
+class ImagesType(BaseModel):
+    images: List[str] = []
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
 capture_rate = int(os.getenv('CAPTURE_RATE', 60))
+basePath = os.getenv('CAPTURE_DIR', 'recording')
 
 lock = threading.Lock()
 
 # App Globals (do not edit)
 app = FastAPI(docs_url=None, redoc_url=None)
+api = FastAPI(title="Sol de Noche", version='0.0.1')
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-templates = Jinja2Templates(directory="templates")
+
+templates = Jinja2Templates(directory="dist")
 
 camera = VideoCamera(flip=False)  # flip pi camera if upside down.
+
+recording = False
 
 t = None
 
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get('/video_feed')
-async def video_feed():
-    return StreamingResponse(generate(), media_type="multipart/x-mixed-replace;boundary=frame")
+def rec_walk(dir):
+    files = []
+    contents = os.listdir(dir)  # read the contents of dir
+    for item in contents:      # loop over those contents
+        if os.path.isdir(dir+'/'+item):
+            # recurse on subdirectories
+            files.extend(rec_walk(dir+'/'+item))
+        else:
+            print(dir+'/'+item)
+            files.append(dir+'/'+item)
+    return files
 
 
 def generate():
@@ -56,33 +89,70 @@ def generate():
 
 
 def start_stream():
-    global camera
-    print('Start stream')
-
-    capture_start = timer()
+    global camera, recording
+    print('Start stream ' + str(recording))
     while True:
-        #capture_end = int(math.floor(timer() - capture_start))
-        # if capture_end >= capture_rate:
-        print('Should write a frame in %0.03ds' % capture_rate)
-        dirPath = 'templates/'+time.strftime("%Y-%m-%d")
+        if recording:
+            print('Should write a frame in %0.03ds' % capture_rate)
+            dirPath = basePath+"/"+time.strftime("%Y-%m-%d")
 
-        if not os.path.isdir(dirPath):
-            print('The directory is not present. Creating a new one..')
-            os.mkdir(dirPath)
-        with lock:
-            mr = open(dirPath+'/'+time.strftime("%H-%M")+".jpg", 'wb+')
-            mr.write(camera.get_frame())
-            mr.close()
-        #capture_start = timer()
-        time.sleep(capture_rate)
+            if not os.path.isdir(dirPath):
+                print('The directory is not present. Creating a new one..')
+                os.mkdir(dirPath)
+            with lock:
+                mr = open(dirPath+'/'+time.strftime("%H-%M")+".jpg", 'wb+')
+                mr.write(camera.get_frame())
+                mr.close()
+            time.sleep(capture_rate)
+
+
+@api.get("/status",)
+async def status():
+    global recording
+    return recording
+
+
+@api.post("/status",)
+async def status(status: StatusType):
+    global recording
+    recording = status.status
+    return recording
+
+
+@api.get("/images", response_model=ImagesType)
+async def images():
+    response = ImagesType(images=rec_walk(basePath))
+
+    return response
+
+app.mount("/api", api)
+
+# Static files configuration
+app.mount("/css", StaticFiles(directory="dist/css"), name="static")
+app.mount("/img", StaticFiles(directory="dist/img"), name="static")
+app.mount("/js", StaticFiles(directory="dist/js"), name="static")
+app.mount("/fonts", StaticFiles(directory="dist/fonts"), name="static")
+app.mount("/recording", StaticFiles(directory=basePath), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get('/video_feed')
+async def video_feed():
+    return StreamingResponse(generate(), media_type="multipart/x-mixed-replace;boundary=frame")
 
 
 @app.on_event("startup")
 async def startup_event():
+    print('Do nothing')
     t = threading.Thread(target=start_stream, )
     t.start()
 
 
 @app.on_event("shutdown")
-def shutdown_event():
+async def shutdown_event():
+    print('Do nothing')
     t.stop()
