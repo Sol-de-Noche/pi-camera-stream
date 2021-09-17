1aa4978a3af86d82d1e24ccf47d1756f580ad286
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2021-09-17 17:33:00 +0200

    Added script to generate videos

---
+++ gen_videos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir -p videos
+BASE_DIR=`pwd`
+TARGET_DIR=$BASE_DIR'/videos/'
+echo $TARGET_DIR
+
+for directory in recording/*; do
+    dirname=`basename $directory`
+    video_name=$dirname'.mp4'
+    echo $directory
+    cd $directory
+    ls *.jpg | sed "s/^/file '/;s/$/'/" > files.txt
+    ffmpeg -f concat -i files.txt $TARGET_DIR$video_name
+    rm files.txt
+    cd -
+done;
