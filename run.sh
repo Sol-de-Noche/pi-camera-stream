8c5565b7efc82bd15943685568c8488c9d542772
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2021-09-20 17:08:59 +0200

    Configure automatic detection of poetry exec

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 BASE_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 RECORDING_DIR=$BASE_DIR'/recording/'
 VIDEOS_DIR=$BASE_DIR'/videos/'
+POETRY=`which poetry`
 
 mkdir -p $RECORDING_DIR
 mkdir -p $VIDEOS_DIR
 
 cd $BASE_DIR
-poetry run uvicorn main:app --host 0.0.0.0 --port 8023
+$POETRY run uvicorn main:app --host 0.0.0.0 --port 8023
