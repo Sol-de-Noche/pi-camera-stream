b6c5c27a19ed200024733df51b57bb7fa20bb0e1
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2021-10-05 17:16:50 +0200

    Updated dev script and camera service

--- run-dev.sh
+++ run-dev.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-mkdir -p recording
+BASE_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+RECORDING_DIR=$BASE_DIR'/recording/'
+VIDEOS_DIR=$BASE_DIR'/videos/'
+POETRY=`which poetry`
+
+mkdir -p $RECORDING_DIR
+mkdir -p $VIDEOS_DIR
 poetry install
 yarn install
 
 yarn serve &
-uvicorn main:app --reload
+$POETRY run uvicorn main:app --reload
